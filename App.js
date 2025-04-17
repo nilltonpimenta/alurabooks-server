c9c3431c132e90f4b09ae9a82e5ef4ab1e9eb6e5
Nillton Pimenta <njqp@icomp.ufam.edu.br>
2025-04-17 18:41:24 -0400

    cors

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routerLivro = require("./routes/livro");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.use("/livros", routerLivro);
 
